29b95dfc5a7c76e740862dfa1b58dd3e81b56ff4
Ashab <ashab340.abidi@gmail.com>
2025-01-24 19:25:51 +0530

    updates

--- client/app/testItems/page.jsx
+++ client/app/testItems/page.jsx
@@ -1,13 +1,6 @@
 "use client"
 import React from 'react'
-import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import Banner1 from "@/public/assets/1.png"
-import Banner2 from "@/public/assets/2.png"
-import Banner3 from "@/public/assets/3.png"
-import Banner4 from "@/public/assets/4.png"
-import Image from 'next/image';
-import CardComponent from '@/sharedComponent/CardComponent';
 import HorizontalCardComponent from '@/sharedComponent/HorizontalCardComponent';
 import CategoryComponent from '@/components/CategoryComponent';
 import Footer from '@/components/Footer';
--- client/lib/connection.js
+++ client/lib/connection.js
@@ -3,14 +3,14 @@ import axios from "axios";
 // Login
 export const loginValidate = async (formData) =>{
 
-    const res = await axios.post("http://localhost:4554/auth/login", formData);
+    const res = await axios.post("https://kritcart.onrender.com/auth/login", formData);
     return res.data;          
 }
 
 // Registration 
 export const registerValidate = async (formData) =>{
     try{
-        await axios.post("http://localhost:4554/auth/register", formData);
+        await axios.post("https://kritcart.onrender.com/auth/register", formData);
 
     }catch(err){
         console.log("Register Error: ",err);
